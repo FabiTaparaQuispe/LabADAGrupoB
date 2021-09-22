6b2fa8ecc13b86b8f0baac540977124b6e73eede
Fabiola Tapara <82911419+FabiTaparaQuispe@users.noreply.github.com>
2021-09-22 10:45:43 -0500

    Coloque un caso prueba al ejercicio3

--- Clase2 20-09-21/Ejercicio3.java
+++ Clase2 20-09-21/Ejercicio3.java
@@ -6,31 +6,39 @@ public class Ejercicio3 {
 	 * por insercion y mostrarlo ordenado */
 	public static void main(String[] args) {
 
-		Scanner s= new Scanner (System.in);
-		int a[],n,k,t;
-		System.out.println("Digite el numero de elementos del arreglo:  ");
-		n = s.nextInt();
-		a=new int[n];
-		System.out.println("Digite el arreglo: ");
-		for(int i=0;i<n;i++) {
-			System.out.print((i+1)+". Digite un numero: ");
-			a[i]=s.nextInt();
-		}
+		// Metodo main del programa
 
-		for(int i=0;i<n;i++) {
+		int[] a = { 10, 4, 8, 9, 1, 15, 7, 5, 13, 6 };
+		System.out.println("\nArreglo inicial: \n");
+		print(a);
+		insertion(a);
+		System.out.println("\nArreglo ordenado: \n");
+		print(a);
+
+	}
+
+	public static void insertion(int[] a) {
+
+		/* Metodo insertion ordenar un arreglo de numeros usando ordenacion
+		 * por insercion*/
+		int k, t;
+		for(int i=0;i<a.length;i++) {
 			k = i;
 			t = a[i];
 			while(k	>0 && a[k-1]>t) { 
 				a[k]=a[k-1];
 				k--;
-
 			}
 			a[k]=t;
 		}
-		System.out.print("\nOrden Ascendente: ");
-		for(int i=0;i<n;i++) {
-			System.out.print(a[i]+" - ");
+
+	}
+
+	public static void print(int[] a) {
+		for(int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + ", "); 
 		}
-		System.out.println("");
+		System.out.println();
+
 	}
 }
