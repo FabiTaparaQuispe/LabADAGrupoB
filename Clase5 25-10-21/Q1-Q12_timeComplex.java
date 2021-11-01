867ab09e2294e2764793c4d68d0e7cf17962d342
Fabiola Tapara <82911419+FabiTaparaQuispe@users.noreply.github.com>
2021-10-31 22:12:04 -0500

    Q2 Time Complejidad

--- Clase5 25-10-21/Q1-Q12_timeComplex.java
+++ Clase5 25-10-21/Q1-Q12_timeComplex.java
@@ -3,9 +3,6 @@ package Ejercicios_Complejidad;
  * Question N1
  * Autor: Fabiola Tapara Quispe
  * Description: Review of Time Complexity
- *
- * Q1: What is the time complexity of? O(n)
- * Q2: What is the time complexity of? O(n)
  * Q3: What is the time complexity of? O(n)
  * Q4: What is the time complexity of? O(n^2)
  * Q5: What is the time complexity of? O(n^2)
@@ -25,30 +22,35 @@ package Ejercicios_Complejidad;
  * for (i = 1; i < n; i=i*3)     // O(log_3(n))
  * for (i = n; i >= 1; i=i/2)    // O(log_2(n))
  */
-public class Q1-Q12_timeComplex {
+public class Q1_lineal {
 	public static void main(String[] args) {
 		System.out.println("Ejercicio1");
 		Q1lineal(2);
 		Q1lineal(8);
+		System.out.println();
 		System.out.println("Ejercicio2");
 		Q2lineal(2);
 		Q2lineal(8);
-		
+		System.out.println();
+		System.out.println("Ejercicio3");
 	}
 
 	// A1: O(n)
 	public static void Q1lineal(int n) {
+		//Q1: What is the time complexity of? O(n)
+		
 		int contador=0;
 		for (int i = 0; i < n; i++) { // Se ejecuta "n+1" veces O(n)
 		    contador++;   // Se ejecuta "n" veces         
 		}
 		System.out.println("Acumulado: " + contador);
 	}
-	// A1: O(n)
+	// A2: O(n)
 	public static void Q2lineal(int n) {
+		//Q2: What is the time complexity of? O(n)
 		int contador=0;
-		for (int i = 0; i < n; i++) { // Se ejecuta "n+1" veces O(n)
-		    contador++;   // Se ejecuta "n" veces         
+		for (int i = n; i > 0; i--) { // Decrementa i-- O(n)
+		    contador++;   // O(1)        
 		}
 		System.out.println("Acumulado: " + contador);
 	}
