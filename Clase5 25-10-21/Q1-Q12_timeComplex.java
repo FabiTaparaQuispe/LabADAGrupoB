5ed5bc701c7bd0a7121bdc4f2161a4ac656a81a6
Fabiola Tapara <82911419+FabiTaparaQuispe@users.noreply.github.com>
2021-11-01 10:49:57 -0500

    Actualice ejercicio 11

--- Clase5 25-10-21/Q1-Q12_timeComplex.java
+++ Clase5 25-10-21/Q1-Q12_timeComplex.java
@@ -55,6 +55,11 @@ public class Q1_lineal {
 		Q10Linel2For(2);
 		Q10Linel2For(8);
 		Q10Linel2For(16);
+		System.out.println();
+		System.out.println("Ejercicio11");
+		Q11LogLogTime(2);
+		Q11LogLogTime(8);
+		Q11LogLogTime(16);
 	}
 
 	// A1: O(n)
@@ -279,10 +284,44 @@ public class Q1_lineal {
 		//obteniendo como resultado final O( 2 * n ), quitando la constante seria O(n)
 		//A10: O(n)
 	}
-	public static void Q11Log(int n) {
+	public static void Q11LogLogTime(int n) {
 		//Q11: What is the time complexity of? O(log log(n))
 		int contador=0;
-
+		int p = 0;
+		for(int i = 1; i < n; i = i * 2) { // For "log_2(n)"
+			p++;	// Acumula en p 
+		}
+		System.out.println("Acumulado: " + p);
+		for(int j = 1; j < p; j = j * 2) { // For "log_2(p)"
+			contador++;	// O(1)
+		}
+		System.out.println("Acumulado: " + contador);
+		/*Explicacion: 
+		 *
+		 *En el primer for: 
+		 *	    i = n / 2 ^ k
+		 * 				i >= 1
+		 * 				i = 1
+		 * 		   	n / 2 ^ k = 1
+		 * 					n = 2 ^ k
+		 * 			  Log2(n) = k
+		 * 			  Log2(n) = p
+		 *El valor de "p" sera el tiempo de la recurrencia
+		 *es decir log_2(n)
+		 *
+		 *En el caso del segundo for: 
+		 *      j = p / 2 ^ q
+		 * 				j >= 1
+		 * 				j = 1
+		 * 		   	p / 2 ^ q = 1
+		 * 			  Log2(n) = 2 ^ q
+		 * 			  Log2(Log2(n)) = q
+		 *Su valor depende del primer for, su tiempo es de log_2(p)
+		 *Finalmente seria: log(p) = log(log(n))
+		 
+		 *A11: O(log log(n))
+		 */
+		
 	}
 	public static void Q12Log(int n) {
 
