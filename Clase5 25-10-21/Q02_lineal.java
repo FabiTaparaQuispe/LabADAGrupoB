4146b97af4d461921e8ee8c2a5419ec1aa7409c6
Fabiola Tapara <82911419+FabiTaparaQuispe@users.noreply.github.com>
2021-11-09 09:34:40 -0500

    Actualice el Ejercicio2 agregando un statement

--- Clase5 25-10-21/Q02_lineal.java
+++ Clase5 25-10-21/Q02_lineal.java
@@ -3,27 +3,30 @@ package Clase5 25-10-21;
  * Question N2
  * Autor: Fabiola Tapara Quispe
  * Description: Review of Time Complexity
- * 				Implementar el ejercicio 2 del algoritmos 
+ * Implementar el ejercicio 2 del algoritmos 
  * visto en clase para calcular el tiempo computacional
  * Date: 25 october
  */
-public class Q002_lineal {
+public class Q02_lineal {
 	public static void main(String[] args) {
-		//Casos de prueba Complexity Time
-		System.out.println("Ejercicio2");
+		System.out.println("Casos de prueba Complexity Time");
+		System.out.println();
+		System.out.println("Ejercicio1");
+		System.out.println("con n=2");
 		Q2lineal(2);
+		System.out.println();
+		System.out.println("con n=4");
 		Q2lineal(4);
-		Q2lineal(8);
-		Q2lineal(16);
 	}
-	// A2: O(n)
+	
 	public static void Q2lineal(int n) {
 		//Q2: What is the time complexity of? O(n)
-   		 //Codigo individual
+		// A2: O(n)
 		int contador=0;
 		for (int i = n; i > 0; i--) { // Decrementa i-- O(n)
-			contador++;   // O(n)        
+			contador++;   // O(n)  
+			System.out.println(i);
 		}
-		System.out.println("Acumulado: " + contador);
+		System.out.println("Numeros: " + contador);
 	}
 }
