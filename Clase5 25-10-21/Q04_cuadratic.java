db1971d8a85278a1058dfa794b3d69d6a62a5026
Fabiola Tapara <82911419+FabiTaparaQuispe@users.noreply.github.com>
2021-11-09 09:41:52 -0500

    Agregue un caso de prueba y un statement

--- Clase5 25-10-21/Q04_cuadratic.java
+++ Clase5 25-10-21/Q04_cuadratic.java
@@ -3,19 +3,20 @@ package Clase5 25-10-21;
  * Question N4
  * Autor: Fabiola Tapara Quispe
  * Description: Review of Time Complexity
- * 				Implementar el ejercicio 4 del algoritmos 
+ * 		Implementar el ejercicio 4 del algoritmos 
  * visto en clase para calcular el tiempo computacional
  * Date: 25 october
  */
-public class Q004_cuadratic {
+public class Q04_cuadratic {
 	public static void main(String[] args) {
 		System.out.println("Casos de prueba Complexity Time");
 		System.out.println();
-		System.out.println("Ejercicio4");
+		System.out.println("Ejercicio8");
+		System.out.println("con n=2");
 		Q4Cuadratic(2);
-		Q4Cuadratic(4);
-		Q4Cuadratic(8);
-		Q4Cuadratic(16);
+		System.out.println();
+		System.out.println("con n=6");
+		Q4Cuadratic(6);
 	}
 	
 	public static void Q4Cuadratic(int n) {
@@ -24,10 +25,11 @@ public class Q004_cuadratic {
 		int contador=0;
 		for(int i = 0; i < n; i++) { // Bucle externo for 'n+1'		
 			for(int j = 0; j < n; j++) { // Bucle interno for 'n*(n+1)'				
-				contador++;	// Acumular n*n				
+				contador++;	// Acumular n*n	
+				System.out.println(i + " - " + j);
 			}
 		}
 		//O(n*(n+1))+(n+1) = O(n^2 + n) = O(n^2)
-		System.out.println("Acumulado: " + contador);
+		System.out.println("Numeros repetidos: " + contador);
 	}
 }
