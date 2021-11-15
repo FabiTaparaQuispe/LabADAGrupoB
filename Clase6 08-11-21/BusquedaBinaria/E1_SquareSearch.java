08cc4c58c160a9a9ea7d58ca03b9c24991bdc327
Fabiola Tapara <82911419+FabiTaparaQuispe@users.noreply.github.com>
2021-11-15 18:03:50 -0500

    Corregi el ejercicio1
    Para que funcione con cualquier numero

--- Clase6 08-11-21/BusquedaBinaria/E1_SquareSearch.java
+++ Clase6 08-11-21/BusquedaBinaria/E1_SquareSearch.java
@@ -7,68 +7,42 @@ import java.util.Scanner;
  *              para comprobar si un numero es cuadrado o no.
  * Date: 08/11/21 
  */
+
 class E1_SquareSearch {
-	//Caso de prueba del ejercicio
 	public static void main(String args[]) {
-		int[] array = { 2, 3, 6, 6, 7, 9, 15, 19 };
 		Scanner s = new Scanner(System.in);
 		System.out.println("Ingrese el valor a buscar: ");
 		int numberSearch = s.nextInt();
-		int binarySearch = binarySearch(array, numberSearch); 
-		//Para corroborar si el numero esta en el array
-		int acumulado = 0;
-		int n = 0;
-		if(binarySearch == -1) { //Si no se encuentra el numero en el array
-			System.out.println("No se encontro " + numberSearch);
+		System.out.println((binarySearch(numberSearch)));
 
-		} else { // mediante sumatoria de numeros impares se corrobora si es cuadrado		
-			while(acumulado <= binarySearch) {
-				if(acumulado == binarySearch) {
-					// Si el numero buscado es igual a una suma acumulada
-					System.out.println("El numero "+ numberSearch + " es cuadrado");
-					break;		
-				} else {
-					n++;
-					/*  Para dar solucion al problema se presenta el siguiente ejemplo
-					 *  de numeros cuadrados: n^2=1+3+5+...+2n-1
-					 */
-					acumulado = acumulado + (2*n - 1);
-				}
-			}
-
-			if(acumulado > binarySearch) {//ultimo caso
-				//El numero se encuentra en el arreglo pero no es cuadrado
-				System.out.println("El numero "+ numberSearch + " no es cuadrado");
-				binarySearch = -1;
-			}
-		}
 	}
-	
-	public static int binarySearch(int[] array, int numberSearch) {
+
+	public static int binarySearch(int numberSearch) {
 		/*  Este metodo recibe un arreglo y el numero a buscar
 		 *  devuelve "-1" si no se encuentra el numero.
 		 */
 		int mid=0;
 		int l = 0;
-		int r = array.length-1;
+		int r = numberSearch;
 		while (l <= r) {	
-			mid = l + (r - l) / 2; // punto medio del arreglo
-			if (array[mid] == numberSearch) {
-				// Si el numero "mid" es igual al numeroBuscado se encontro
-				System.out.print("Se encontro el numero ");
+			mid = l + (r - l) / 2; // punto medio
+			if (mid*mid == numberSearch) {
+				// Si "mid"*"mid" es igual al numeroBuscado es cuadrado
+				System.out.print("El numero "+numberSearch+" es cuadrado");
 				System.out.println();
-				return array[mid];
+				return numberSearch;
 			}
 
-			if (array[mid] < numberSearch)
-				// Si el numero "mid" es menor, buscar en la mitad superior
+			else if (mid*mid < numberSearch)
+				// Si el numero "mid*mid" es menor, buscar en la mitad superior
 				l = mid + 1;
 			else
 				// Si el numero "mid" es mayor, buscar en la mitad inferior
 				r = mid - 1;
 		}
-		System.out.println("No se encontro el numero " + numberSearch);
+		System.out.println("No es cuadrado " + numberSearch);
 
-		return -1; // si no se encuentra el numero buscado
+		return -1; // si no es cuadrado el numero buscado
 	}
+	
 }
