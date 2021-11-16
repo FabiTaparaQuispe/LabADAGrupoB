dbed1635ff2e8449dde761e3909eb1504b05591b
Fabiola Tapara <82911419+FabiTaparaQuispe@users.noreply.github.com>
2021-11-15 22:57:53 -0500

    Actualice el ejercicio 2 Inmediato mayor
    para que busque el primer valor mayor o igual

--- Clase6 08-11-21/BusquedaBinaria/E2_InmediatoMayor.java
+++ Clase6 08-11-21/BusquedaBinaria/E2_InmediatoMayor.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
  * Ejercicio 2 InmediatoMayor
  * Autor: Fabiola Tapara Quispe
  * Description: Implementar el algoritmo de busqueda binaria 
- *              y devolver el numero inmediato mayor al buscado
+ *              y devolver el numero inmediato mayor o igual al buscado
  * Date: 08/11/21 
  */
+
+import java.util.Scanner;
 
 public class E2_InmediatoMayor {
 	public static void main(String args[]) {
@@ -14,7 +16,18 @@ public class E2_InmediatoMayor {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Ingrese el valor a buscar: ");
 		int numberSearch = s.nextInt();
-		System.out.println((inmediatoMayorSearch(array, numberSearch)));
+		int value=(inmediatoMayorSearch(array, numberSearch));
+		if (value == -1)
+			System.out.println("No se encontró");
+		else {
+			System.out.print("El inmediato igual o mayor es: "+value);
+		}
+		//casos de prueba
+		System.out.println();
+		System.out.println("Mayor o igual a 10: " + inmediatoMayorSearch(array, 10) + "\n");
+		System.out.println("Mayor o igual a 19: " + inmediatoMayorSearch(array, 19) + "\n");
+		System.out.println("Mayor o igual a 6: " + inmediatoMayorSearch(array, 6) + "\n");
+		System.out.println("Mayor o igual a 0: " + inmediatoMayorSearch(array, 0) + "\n");
 
 	}
 	public static int inmediatoMayorSearch(int[] array, int numberSearch) {
@@ -29,21 +42,26 @@ public class E2_InmediatoMayor {
 			//System.out.println("Indice medio: " +mid);
 			if (array[mid] == numberSearch) {
 				// Si el numero "mid" es igual al numeroBuscado se encontro
-				System.out.print("El inmediato Mayor es: ");
-				return array[mid+1];
+				
+				return array[mid];
 			}
 
-			else if (array[mid] < numberSearch) {
-				// Si el numero "mid" es menor, buscar en la mitad superior
-				//System.out.println("Comparando es menor");
-				l = mid + 1;
-			}else {
-				// Si el numero "mid" es mayor, buscar en la mitad inferior
-				//System.out.println("Comparando es mayor");
-				r = mid - 1;
+			else {
+				if (numberSearch<array[mid]) {
+					// Si el numero "mid" es mayor, buscar en la mitad inferior
+					//System.out.println("Comparando es mayor");
+					r = mid - 1;
+				}else {
+
+					// Si el numero "mid" es menor, buscar en la mitad superior
+					//System.out.println("Comparando es menor");
+
+					l = mid + 1;
+				}
 			}
 
 		}
-	    return array[mid+1];
+		return array[mid];
 	}
 }
+
