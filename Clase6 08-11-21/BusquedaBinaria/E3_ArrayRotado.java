bd7ff63fb7a3798d245b0e7cb3836013c68c107d
Fabiola Tapara <82911419+FabiTaparaQuispe@users.noreply.github.com>
2021-11-16 23:39:31 -0500

    Actualice el ejercicio de ArrayRotated
    Para mas casos de prueba con arrays ordenados y no ordenados

--- Clase6 08-11-21/BusquedaBinaria/E3_ArrayRotado.java
+++ Clase6 08-11-21/BusquedaBinaria/E3_ArrayRotado.java
@@ -10,27 +10,34 @@ package Clase6 08-11-21;
 
 public class E3_ArrayRotado {
 	public static void main(String args[]) {
-		//casos de prueba
 		int[] array1 = { 2, 3, 6, 6, 7, 9, 15, 19 };
-		int[] array2 = {1, 3, 5, 7, 9, 11, 13, 18};
+		int[] array2 = {9, 11, 14, 20, 4, 5, 6, 7};
+		int[] array3 = {6,8,10,12,1,2,3,5};
 
 		System.out.println("menor elemento de array 1 es: "+(lessSearch(array1)));
+		System.out.println("Resultado esperado: 2");
 		System.out.println("menor elemento de array 2 es: "+(lessSearch(array2)));
-
+		System.out.println("Resultado esperado: 4");
+		System.out.println("menor elemento de array 3 es: "+(lessSearch(array3)));
+		System.out.println("Resultado esperado: 1");
 	}
 
 	public static int lessSearch(int [] array){
-		// Este metodo buscar el menor elemento dentro de un array rotado
 		int l = 0;
 		int r = array.length-1;
 		int mid = 0;
 		while(l <= r){
 			mid = ( l + r ) / 2;
-			if(array[l] < array[r]){  
+			if(array[l] < array[r]){ 
 				r = mid;
 			}
 			else if(array[l] > array[r]){
-				l = mid;
+				if(array[l +1 ] < array[mid]){ 
+					l = l +1;
+				}
+				else {
+					l = mid;
+				}
 			}
 			if(l == r){
 				return array[mid];
@@ -39,4 +46,5 @@ public class E3_ArrayRotado {
 		return array[mid]; 
 	}
 }
+
 
