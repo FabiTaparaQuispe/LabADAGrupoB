81bdf07a7a278787ae5faf85abf5a1afb428c7ba
Fabiola Tapara <82911419+FabiTaparaQuispe@users.noreply.github.com>
2021-11-19 18:42:35 -0500

    Cree una carpeta para los ejercicios

---
+++ Clase7 15-11-2021/Stack/reversePolish.java
@@ -0,0 +1,10 @@
+/* 
+ * Ejercicio 1 reverse Polish Notation
+ * Autor: Fabiola Tapara Quispe
+ * Description:  Mediante el siguiente metodo se recibe un string con datos y signos de las operaciones
+ * basicas de la matematica
+ * Date: 08/11/21 
+ */
+class reversePolish {
+
+}
