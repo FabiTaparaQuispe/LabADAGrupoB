3e997c6bd6128e008037ee7616dba000dfba7e50
Fabiola Tapara <82911419+FabiTaparaQuispe@users.noreply.github.com>
2021-11-19 17:02:28 -0500

    Actualice el ejercicio usando simbolos "#" "-" "o"

--- Clase6 08-11-21/GRAPHS_DFS/E4_DFS_matriz.cpp
+++ Clase6 08-11-21/GRAPHS_DFS/E4_DFS_matriz.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 // clase que arma la matriz que delimita el espacio 
-void print(int matriz[][6],int filas, int columnas){
+void print(char matriz[][6],int filas, int columnas){
    for(int i=0;i<filas;i++){ //recorre las filas de la matriz
       for(int j=0;j<columnas;j++){ // recorre las columnas de la matriz
          cout<<matriz[i][j]<<" ";
@@ -19,12 +19,13 @@ void print(int matriz[][6],int filas, int columnas){
 }
 // clase que llena la matriz, recibe un vector de tamanio de columnas de 6, el numero
 // de filas y columnas y sus posiciones asi como su desplazamiento x,y
-void llenadoPri(int matriz[][6],int& filas, int& columnas, int& posx,int& posy,int avax,int avay){
-   if(matriz[avax+posx][avay+posy]!=1 && matriz[avax+posx][avay+posy]!=2){
+
+void llenadoPri(char matriz[][6],int& filas, int& columnas, int& posx,int& posy,int avax,int avay){
+   if(matriz[avax+posx][avay+posy]!='#' && matriz[avax+posx][avay+posy]!='o'){
      //si las posiciones de la matriz es diferente de 1 y diferente de dos, 
       posx=avax+posx;//al elemento de la posicion x se le agrega una posicion x
       posy=avay+posy;//al elemento de la posicion y se le agrega una posicion y
-      matriz[posx][posy]=2;//la matriz recorre las posiciones "x" y "y"
+      matriz[posx][posy]='o';//la matriz recorre las posiciones "x" y "y"
       cout<<endl;
       print(matriz,6,6); //imprime la posicion final de la matriz de tamanio 6x6
 
@@ -37,17 +38,23 @@ void llenadoPri(int matriz[][6],int& filas, int& columnas, int& posx,int& posy,i
       return ;
    }
 }
+
 //se utiliza el llenado por DFS
-void llenado(int matriz[][6],int filas, int columnas, int posx,int posy){
+void llenado(char matriz[][6],int filas, int columnas, int posx,int posy){
    llenadoPri(matriz,filas,columnas,posx ,posy,0,0);
 }
 
 //clase main para realizar los casos de prueba
 int main(){ 
-   int matriz[][6]={{1,1,1,1,1,1},{1,0,0,0,0,1},{1,0,0,0,0,1},{1,0,0,0,0,1},{1,0,0,0,0,1},{1,1,1,1,1,1}};
+   char matriz[][6]={{'#','#','#','#','#','#'},
+                     {'#','-','-','-','-','#'},
+                     {'#','-','-','-','-','#'},
+                     {'#','-','-','-','-','#'},
+                     {'#','-','-','-','#','-'},
+                     {'#','#','#','#','-','-'}};
   //matriz que indica "0" aun no ha sido visitado y "1" que ya ha sido visitado
    print(matriz,6,6); 
-   llenado(matriz,6,6,5,5);//imprime la matriz con los numeros completos
+   llenado(matriz,6,6,1,1);//imprime la matriz con los numeros completos
 
    return 0;
 }
