cb7ce7affb996081a8eeaa5e2e3da6058d8e9027
Fabiola Tapara <82911419+FabiTaparaQuispe@users.noreply.github.com>
2021-11-22 19:12:39 -0500

    casos de prueba

--- Clase6 08-11-21/BusquedaBinaria/E1_BinarySearch.java
+++ Clase6 08-11-21/BusquedaBinaria/E1_BinarySearch.java
@@ -1,20 +1,28 @@
 package Clase6 08-11-21;
-import java.util.Scanner;
+
 /* 
  * Ejercicio 1 BynarySearch
  * Autor: Fabiola Tapara Quispe
  * Description: Implementar el algoritmo de busqueda binaria 
  *              y buscar un numero indicando si se encuentra o no. 
  * Date: 08/11/21 
-*/
+ */
 
 class E1_BinarySearch {
 	public static void main(String args[]) {
 		int[] array = { 2, 3, 6, 6, 7, 9, 15, 19 };
-		Scanner s = new Scanner(System.in);
-		System.out.println("Ingrese el valor a buscar: ");
-		int numberSearch = s.nextInt();
-		System.out.println((binarySearch(array, numberSearch)));
+		int prueba1 = 20;
+		int prueba2 = 7;
+		int prueba3 = 50;
+		
+		System.out.println("Buscando el numero: " + prueba1 + " ... ");
+		System.out.println(binarySearch(array, prueba1));
+		
+		System.out.println("Buscando el numero: " + prueba2 + " ... ");
+		System.out.println(binarySearch(array, prueba2));
+		
+		System.out.println("Buscando el numero: " + prueba3 + " ... ");
+		System.out.println(binarySearch(array, prueba3));
 
 	}
 	public static int binarySearch(int[] array, int numberSearch) {
@@ -28,7 +36,7 @@ class E1_BinarySearch {
 			mid = l + (r - l) / 2; // punto medio del arreglo
 			if (array[mid] == numberSearch) {
 				// Si el numero "mid" es igual al numeroBuscado se encontro
-				System.out.print("Se encontro el numero ");
+				System.out.print("Si esta el numero ");
 				return array[mid];
 			}
 
@@ -39,7 +47,7 @@ class E1_BinarySearch {
 				// Si el numero "mid" es mayor, buscar en la mitad inferior
 				r = mid - 1;
 		}
-		System.out.println("No se encontro el numero " + numberSearch);
+		System.out.println("No esta el numero " + numberSearch);
 
 		return -1; // si no se encuentra el numero buscado
 	}
