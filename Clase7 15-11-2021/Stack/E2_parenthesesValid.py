550252b88a39d457bd6794942b8b010b83e138d9
Fabiola Tapara <82911419+FabiTaparaQuispe@users.noreply.github.com>
2021-11-22 19:59:50 -0500

    Actualice ejercicio con pila

---
+++ Clase7 15-11-2021/Stack/E2_parenthesesValid.py
@@ -0,0 +1,37 @@
+''''
+ * Ejercicio 2 MinimumAddtoMakeParenthesesValid
+ * Autor: Fabiola Tapara Quispe
+ * Description: Verificar el ingreso correcto de parentesis, apertura y cierre,
+ * mostrar la cantidad de aquellos que no lo estan usando una Pila.
+ * Date: 15/11/21 
+'''
+abreParentesis = ["("]
+cierraParentesis = [")"]
+
+def parenthesisValid(str):
+    lista = []
+    for i in str:
+        print(i)
+        if(i in abreParentesis):#si es "("
+            lista.append(i)
+        elif(i in cierraParentesis):
+            if(len(lista) == 0 or lista[-1] in cierraParentesis):
+                lista.append(i)
+            elif(lista[-1] in abreParentesis):
+                lista.pop()
+    return lista
+        
+        
+
+## caso de prueba
+input1 = parenthesisValid("())")
+print(input1)
+print(len(input1))
+
+input3 = parenthesisValid("(()))(")
+print(input3)
+print(len(input3))
+
+input3 = parenthesisValid("()))((")
+print(input3)
+print(len(input3))
