9ad3ea5ebd73aeec3f8b037b439769f1a6b437c1
Fabiola Tapara <82911419+FabiTaparaQuispe@users.noreply.github.com>
2021-12-02 17:22:48 -0500

    Ejercicio2, falta completarlo

---
+++ Clase8 29-11-2021/E2_BookShop.java
@@ -0,0 +1,14 @@
+package Lab8;
+/* 
+ * Ejercicio 2 Book Shop
+ * Autor: Fabiola Tapara Quispe
+ * Descripcion: Estás en una librería que vende "n" libros diferentes. 
+ * Sabes el precio y el número de páginas de cada libro. Ha decidido
+ * que el precio total de sus compras será como máximo "x". 
+ * Cual es el maximo número de páginas que puedes comprar? 
+ * Puedes comprar cada libro como máximo una vez. 
+ * Fecha: 2/12/21 
+ */
+public class E2_BookShop {
+
+}
